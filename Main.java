ff55dd291911f0338de8c9af92e37eb617052bd1
Vitor Ignacio <vitor20120ignacio@gmail.com>
2021-10-16 15:56:15 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    //leitor
+   Scanner leitor = new Scanner(System.in);
+    //variavel
+    int numero =0;
+    //entrada
+    System.out.println("Escreva o número");
+    numero=leitor.nextInt();
+    //saida
+    System.out.println("número digitado foi "+numero);
+
+  
+
+  }
+}
